f860343f5e7b51615f04a7a9a34e8754405a05fa
yeg2799 <yeg2799@gmail.com>
2021-07-04 14:31:29 +0300

    created content part and ui fixed

--- components/Content.vue
+++ components/Content.vue
@@ -1,15 +1,90 @@
 <template>
-  <div>
-      Content
+  <div class="content">
+      <div class="content-top">
+        <div class="content-top-left">
+          <div class="checkbox"></div>
+          <div class="icon refresh">
+             <svg-sprite icon="refresh" />
+          </div>
+          <div class="icon threedot">
+              <svg-sprite icon="threedotvert"/>
+          </div>
+        </div>
+        <div class="content-top-right">
+          <div class="page-numbers">
+            <span>59 satırdan 1-50 arası</span>
+          </div>
+          <div class="page-transition">
+            <div class="icon left-transition">
+              <svg-sprite icon="leftarrow" />
+            </div>
+            <div class="icon right-transition">
+              <svg-sprite icon="rightarrow" />
+            </div>
+          </div>
+          <div class="icon keyboard">
+            <svg-sprite icon="keyboard"/>
+          </div>
+        </div>
+      </div>
+      <div class="content-bottom" v-for="n in 20" :key="n">
+        <MessageBox />
+      </div>
   </div>
 </template>
 
 <script>
+import MessageBox from './MessageBox.vue'
+
+
 export default {
+  components: { MessageBox },
 
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.content{
+  display: flex;
+  flex-direction: column;
+  
+  .content-top{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 10px 30px;
+    .content-top-left{
+      display: flex;
+      align-items: center;
+      .refresh{
+        margin: 0px 30px;
+      }
+    }
+      .content-top-right{
+      display: flex;
+      align-items: center;
+      .page-transition{
+        display: flex;
+        margin: 0px 30px;
+      }
+    }
+  }
+}
+.content-bottom{
+  overflow: auto;
+}
+.icon{
+  display: flex;
+  svg{
+    width: 20px;
+    height: 20px;
+  }
+}
+.checkbox{
+        width: 18px;
+        height: 18px;
+        border: 1px solid black;
+        display: flex;
+        align-items: center;
+  }
 </style>
---
+++ components/MessageBox.vue
@@ -0,0 +1,63 @@
+<template>
+        <div class="message-box">
+            <div class="left-icons">
+              <div class="checkbox"></div>
+              <div class="star icon">
+                <svg-sprite icon="starempty" />
+              </div>
+              <div class="important icon">
+                <svg-sprite icon="importantempty" />
+              </div>
+              <div class="message-title">
+                  <span>Hepsiburada</span>
+              </div>
+            </div>
+
+            <div class="message-text">
+              <span>Redmi Note 10 5G alana Mi True Wireless Earphone 2 Basic hediye!</span><span>İhtiyaçlarnı, indirimleri kısacası aradığın herşeyi burada bulursun. Vantilatörde %25'e varan indirimler. Yeni nes...</span>
+            </div>
+            <div class="message-date">
+              <span>3 Tem</span>
+            </div>
+          </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.checkbox{
+        width: 18px;
+        height: 18px;
+        border: 1px solid black;
+        display: flex;
+        align-items: center;
+  }
+  .message-box{
+    display: flex;
+    align-items: center;
+    border: 1px solid #F1F3F4;
+    background: #F5F7F7;
+    justify-content: space-between;
+    padding: 10px;
+    margin: 0px 5px;
+    cursor: pointer;
+    &:hover{
+        box-shadow: inset -1px -1px 0px 1px rgb(100 121 143 / 60%);
+    }
+    .left-icons{
+      display: flex;
+      align-items: center;
+      margin-left: 15px;
+      .star{
+        margin: 0 15px;
+      }
+      .important{
+        margin-right: 5px;
+      }
+    }
+  }
+</style>
